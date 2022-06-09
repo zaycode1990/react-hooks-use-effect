2788ce46c4074ce2725d4d4e3227d602217aca73
Isaiah <ijandicos90@gmail.com>
2022-06-08 21:40:54 -0400

    Completed Assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
+
+
   useEffect(() => {
     console.log("useEffect called");
   });
 
   console.log("Component rendering");
+return (
+  <div>
+    <button onClick={()=>setCount((count)=> count +1)}>
+      I've been clicked {count} times </button>
+      <input
+      type="text"
+      placeholder="Type away..."
+      value={text}
+      onChange={(e)=>setText(e.target.value)}
+      />
+  </div>
+)
 
-  return <button>Click Me</button>;
+  
 }
+
+
+
 
 export default App;
